c12317b721ab6a02095efdcc6d7e10880b9dd8c7
Phyo Zaw Hein <phyozawhein1997@gmail.com>
2023-12-27 02:58:38 -0500

    updated origin policy

--- backend/app.js
+++ backend/app.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
  
 app.use((req,res,next)=>{
 
-  res.setHeader("Access-Control-Allow-Origin","http://localhost:3000");
+  res.setHeader("Access-Control-Allow-Origin","*");
   res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PUT');
   res.setHeader(
     'Access-Control-Allow-Headers',
